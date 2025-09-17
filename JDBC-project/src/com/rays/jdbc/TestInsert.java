872db7a73cf7af6630c4b9b02b0cc201984ca496
Rishabh Shrivastava <rishabhshrivastava7565@gmail.com>
2025-09-17 14:56:23 +0530

    new

---
+++ JDBC-project/src/com/rays/jdbc/TestInsert.java
@@ -0,0 +1,31 @@
+package com.rays.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestInsert {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+		// 1. Load Driver
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+		// 2. Make connection to the database
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "root");
+
+		// 3. Create Statement
+		Statement stmt = conn.createStatement();
+
+		// 4. Execute Query and get ResultSet
+		int i = stmt.executeUpdate("insert into marksheet values(18,'chelsi', 25,85,58)");
+		
+
+			System.out.println("Data added successfully: " + i);
+		
+
+		conn.close();
+
+	}
+
+}
